6b0aa42669e45186ba79a84e2008ca8bd56ca1e5
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2021-12-30 18:05:25 +0700

    Up lại thêm comment :).

--- phuongtrinhbac1.cs
+++ phuongtrinhbac1.cs
@@ -5,6 +5,12 @@ namespace ptrinhb1
 {
     class Program
     {
+        /// <summary>
+        /// hàm tính phương trình bậc 1
+        /// </summary>
+        /// <param name="a">Tham số thứ 1</param>
+        /// <param name="b">Tham số thứ 2</param>
+        /// <returns>In ra kết quả nha</returns>
         static string PhuongTrinhBac1 (double a, double b)
         {
             if (a == 0 && b == 0) { return "Phương trình vô số nghiệm";  }

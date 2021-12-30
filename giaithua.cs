f58f5c890b2829ca39afc6403f8433d81cb12347
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2021-12-30 17:44:36 +0700

    Bài tập tính giai thừa.
    và đây là kết quả

---
+++ giaithua.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Text;
+
+namespace vonglap
+{
+    class Program { 
+        /// <summary>
+        /// Tính giai thừa của một số
+        /// </summary>
+        /// <param name="a">Đưa rham số cần tính giai thưa vào</param>
+        /// <returns>kết quả giai thừa</returns>
+        static int TinhGiaiThua(int a)
+        {
+            int gt = 1;
+            for(int i = 1; i <= a; i++)
+            {
+                gt *= i;
+            }
+            return gt;
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nhập số cân tính giai thừa: ");
+            int num;
+            num = int.Parse(Console.ReadLine());
+            int kq;
+            kq = TinhGiaiThua(num);
+            Console.WriteLine("Kết quả giai thừa của số {0} : {1}", num, kq);
+            Console.ReadLine();
+        }
+        
+    }
+}
